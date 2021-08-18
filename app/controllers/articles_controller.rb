e32dc11b3e7bea18bde49c8ca9dbd67b5f8d622e
harmolipi <nikob381@gmail.com>
2021-08-18 10:53:46 -0400

    10.1 basic authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "test", password: "niko", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
